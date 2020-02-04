3921d09001b9671081ae043dc9a9d12de658196d
Eric Wallmander <git@wallmander.nu>
2020-02-04 22:22:49 +0100

    Refactoring, extract to separate class

---
+++ tools/src/AppData.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace Sbj\tools;
+
+
+use DateTime;
+use DateTimeZone;
+use Exception;
+
+class AppData
+{
+    /** @var array User agent mapping */
+    const uaMapping = [
+        'SwedbankMOBPrivate'     => 'swedbank',
+        'SavingbankMOBPrivate'   => 'sparbanken',
+        'SwedbankMOBCorporate'   => 'swedbank_foretag',
+        'SavingbankMOBCorporate' => 'sparbanken_foretag',
+        'SwedbankMOBYouth'       => 'swedbank_ung',
+        'SavingbankMOBYouth'     => 'sparbanken_ung',
+    ];
+
+    /** @var string User agent match pattern */
+    const userAgentPattern = '#^([a-zA-Z]{16,22})(IOS|Android)/[0-9\.]+_#u';
+
+    /** @var string Full path to AppData.json */
+    private $sourcePath;
+
+    /** @var array JSON decoded AppData.json */
+    private $sourceData;
+
+    public function __construct(string $sourcePath) {
+        $this->sourcePath = $sourcePath;
+    }
+
+    /**
+     * AppData.json source file
+     *
+     * @return mixed
+     */
+    protected function getSourceData(): array
+    {
+        if ( !$this->sourceData )
+        {
+            $this->sourceData = json_decode(file_get_contents($this->sourcePath), true);
+        }
+
+        return $this->sourceData;
+    }
+
+
+    /**
+     * Load .chljs file
+     *
+     * @param string $chljsFilePath
+     *
+     * @return mixed
+     * @throws Exception
+     */
+    public function loadChljs( string $chljsFilePath )
+    {
+        if ( !is_readable($chljsFilePath) OR !preg_match('/\.chlsj$/iu', $chljsFilePath) )
+        {
+            throw new Exception("'$chljsFilePath' dose not exists or is not readable");
+        }
+
+        return json_decode(file_get_contents($chljsFilePath));
+    }
+
+    /**
+     * Extract traffic data
+     *
+     * @param object $rawChljs
+     *
+     * @return array
+     */
+    public function parserChljs($rawChljs): array
+    {
+        $misses     = [];
+        $newAppData = [];
+        foreach($rawChljs as $i => $r)
+        {
+            if ( !isset($r->request->header->headers) OR !strpos($r->host, 'api.swedbank.se') )
+            {
+                continue;
+            }
+
+            $temp = [ 'appID' => '', 'useragent' => '', ];
+            foreach($r->request->header->headers as $h)
+            {
+                switch ($h->name)
+                {
+                    case "User-Agent":
+                        $temp['useragent'] = $h->value;
+                        break;
+                    case "Authorization":
+                        $decoded       = base64_decode($h->value);
+                        $temp['appID'] = explode(':', $decoded)[0];
+                        break;
+                    default:
+                        continue 2;
+                }
+
+                if ( !$temp['appID'] OR !$temp['useragent'] )
+                {
+                    continue;
+                }
+                elseif ( !preg_match(self::userAgentPattern, $temp['useragent'], $m) )
+                {
+                    continue 2;
+                }
+
+                $bankType = self::uaMapping[ $m[1] ?? '' ] ?? null;
+
+                if(isset($bankType)) {
+                    $newAppData[ $bankType ] = $temp;
+                    break;
+                }
+                
+                $misses[] = $temp;
+            }
+        }
+
+        return $newAppData;
+    }
+
+    /**
+     * Merge AppData source with imported traffic data
+     *
+     * @param array $importedAppData
+     *
+     * @return array
+     * @throws Exception
+     */
+    public function merge(array $importedAppData): array
+    {
+        $appDataSource = $this->getSourceData();
+
+        $tempNewApps = [];
+        foreach($appDataSource['apps'] as $a => $d)
+        {
+            if ( isset($importedAppData[ $a ]) )
+            {
+                $d = $importedAppData[ $a ];
+            }
+            $tempNewApps[ $a ] = $d;
+        }
+
+        return $tempNewApps;
+    }
+
+    /**
+     * Check if anything have changed
+     *
+     * @param array $newAppData
+     *
+     * @return bool
+     */
+    public function compare( array $newAppData): bool {
+        return $newAppData === $this->getSourceData()['apps'];
+    }
+
+    /**
+     * Update AppData source
+     *
+     * @param array $appData
+     *
+     * @return bool
+     * @throws Exception
+     */
+    public function updateSource( array $appData ): bool
+    {
+        $base               = $this->getSourceData();
+        $newAppData         = $base;
+        $newAppData['apps'] = $appData;
+
+        try
+        {
+            $dt = new DateTime('NOW', new DateTimeZone('Europe/Stockholm'));
+        }
+        catch ( Exception $e )
+        {
+            throw new Exception('DateTime error');
+        }
+
+        $newAppData['meta']['timestamp'] = $dt->getTimestamp();
+        $newAppData['meta']['updated']   = $dt->format(DateTime::ISO8601);
+
+        if ( !file_put_contents($this->sourcePath, json_encode($newAppData, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)) )
+        {
+            throw new Exception('Update detected, but can\'t write to file');
+        }
+
+        return true;
+    }
+}
--- tools/updateAppData.php
+++ tools/updateAppData.php
@@ -1,33 +1,20 @@
 <?php
 
-if (php_sapi_name() != 'cli')
+use Sbj\tools\AppData;
+
+if ( php_sapi_name() != 'cli')
 {
     exit('This script can only be executed in CLI mode');
 }
 
+require_once __DIR__.'/src/AppData.php';
+
 $appDataPath = __DIR__.'/../src/AppData.json';
 $chljsFile = $argv[1] ?: '';
-
-$appsMapping = [
-    'SwedbankMOBPrivate'     => 'swedbank',
-    'SavingbankMOBPrivate'   => 'sparbanken',
-    'SwedbankMOBCorporate'   => 'swedbank_foretag',
-    'SavingbankMOBCorporate' => 'sparbanken_foretag',
-    'SwedbankMOBYouth'       => 'swedbank_ung',
-    'SavingbankMOBYouth'     => 'sparbanken_ung',
-];
-
-// Matching useragent (eg. SwedbankMOBCorporate/0.0.0_) for one of the apps mappings
-$userAgentPattern = '#^([a-zA-Z]{16,22})(IOS|Android)/[0-9\.]+_#u';
 
 if (empty($chljsFile))
 {
     exit('Valid path to a .chljs file is required'.PHP_EOL);
-}
-
-if(!is_readable($chljsFile) OR !preg_match('/\.chlsj$/iu', $chljsFile))
-{
-    exit("'$chljsFile' dose not exists or is not readable".PHP_EOL);
 }
 
 if (!is_writable($appDataPath))
@@ -35,75 +22,26 @@ if (!is_writable($appDataPath))
     exit("Either can't find AppData.json, or the file isn't writable".PHP_EOL);
 }
 
-$trafficSource = json_decode(file_get_contents($chljsFile));
+$ad = new AppData($appDataPath);
 
-$appDataSource = json_decode(file_get_contents($appDataPath), true);
-
-$misses     = [];
-$newAppData = $appDataSource;
-foreach ($trafficSource as $i => $r)
+try
 {
-    if (!isset($r->request->header->headers) OR !strpos($r->host, 'api.swedbank.se'))
+    $trafficSource   = $ad->loadChljs($chljsFile);
+    $importedAppData = $ad->parserChljs($trafficSource);
+
+    $newAppData = $ad->merge($importedAppData);
+    if ( $ad->compare($newAppData) )
     {
-        continue;
+        echo "No difference sense last update".PHP_EOL;
+        exit;
     }
 
-    $temp = ['appID'=>'','useragent'=>'',];
-    foreach ($r->request->header->headers as $h)
+    if ( $ad->updateSource($newAppData) )
     {
-        switch ($h->name)
-        {
-            case "User-Agent":
-                $temp['useragent'] = $h->value;
-            break;
-            case "Authorization":
-                $decoded   = base64_decode($h->value);
-                $temp['appID'] = explode(':', $decoded)[0];
-            break;
-            default:
-                continue 2;
-        }
-
-        if (!$temp['appID'] OR !$temp['useragent'])
-        {
-            continue;
-        }
-        elseif (!preg_match($userAgentPattern, $temp['useragent'], $m))
-        {
-            continue 2;
-        }
-
-        $bankType = $appsMapping[$m[1] ?? ''] ?? null;
-
-        if (isset($newAppData['apps'][$bankType]))
-        {
-            $newAppData['apps'][$bankType] = $temp;
-            break;
-        }
-
-        $misses[] = $temp;
+        echo "Success! $appDataPath updated".PHP_EOL;
     }
 }
-
-if ($newAppData !== $appDataSource)
+catch ( Exception $e )
 {
-    $dt = new DateTime('NOW', new DateTimeZone('Europe/Stockholm'));
-
-    $newAppData['meta']['timestamp'] = $dt->getTimestamp();
-    $newAppData['meta']['updated']   = $dt->format(DateTime::ISO8601);
-
-    if (file_put_contents($appDataPath, json_encode($newAppData, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)))
-    {
-        echo "Success! appdata.json updated".PHP_EOL;
-    }
-    else
-    {
-        echo "Error: Update detected, but can't write to file".PHP_EOL;
-    }
+    echo 'Error: '.$e->getMessage().PHP_EOL;
 }
-else
-{
-    echo "No difference sense {$newAppData['meta']['updated']} update".PHP_EOL;
-}
-
-//print_r($newAppData);
